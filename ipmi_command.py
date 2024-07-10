94cb7b06b7d8a90f043298dc65b41c593b4703e2
atheer <atheerwaled66@hotmail.se>
2024-07-10 22:55:38 +0200

    refactor: change ipmi command to be async

--- ipmi_command.py
+++ ipmi_command.py
@@ -1,5 +1,5 @@
 import subprocess
-import shlex
+import asyncio
 
 from fanspeed_quantities import fanspeed_quantities
 
@@ -12,17 +12,19 @@ IPMIKEY = "0000000000000000000000000000000000000000"
 
 last_fanspeed_quantity_name = None
 
-
-def set_fan_speed(fanspeed_quantity: fanspeed_quantities):
+async def set_fan_speed(fanspeed_quantity: fanspeed_quantities):
 	global last_fanspeed_quantity_name
 	match fanspeed_quantity.value.name:
 		case fanspeed_quantities.AUTO.value.name:
 			command = f"ipmitool -I lanplus -H {IPMIHOST} -U {IPMIUSER} -P {IPMIPASSWORD} -y {IPMIKEY} raw 0x30 0x30 0x01 0x01"
-			result = subprocess.run(shlex.split(command), capture_output=True, text=True)
+			proc = await asyncio.create_subprocess_shell(command, stderr=subprocess.PIPE)
 
-			if result.stderr:
+			_, stderr = await proc.communicate()
+			stderr = stderr.decode("utf-8")
+
+			if stderr:
 				print(
-					f'An error has occured when setting fan speed to "{fanspeed_quantity.value.name}" with the following message \n {result.stderr}'
+					f'An error has occured when setting fan speed to "{fanspeed_quantity.value.name}" with the following message \n {stderr}'
 				)
 
 			if last_fanspeed_quantity_name != fanspeed_quantities.AUTO.value.name:
@@ -36,19 +38,23 @@ def set_fan_speed(fanspeed_quantity: fanspeed_quantities):
 
 				# setting manual control
 				command = f"ipmitool -I lanplus -H {IPMIHOST} -U {IPMIUSER} -P {IPMIPASSWORD} -y {IPMIKEY} raw 0x30 0x30 0x01 0x00"
-				result = subprocess.run(shlex.split(command), capture_output=True, text=True)
+    
+				proc = await asyncio.create_subprocess_shell(command, stderr=subprocess.PIPE)
+				_, stderr = await proc.communicate()
 
 				print("setting manual control")
-				if result.stderr:
+				if stderr:
 					print(
-						f"An error has occured when enabling manual fanspeed (required) to set custom pwm value with the following message \n {result.stderr}"
+						f"An error has occured when enabling manual fanspeed (required) to set custom pwm value with the following message \n {stderr}"
 					)
 
 			pwm = fanspeed_quantity.value.pwm
 			command = f"ipmitool -I lanplus -H {IPMIHOST} -U {IPMIUSER} -P {IPMIPASSWORD} -y {IPMIKEY} raw 0x30 0x30 0x02 0xff {pwm}"
-			result = subprocess.run(shlex.split(command), capture_output=True, text=True)
 
-			if result.stderr:
+			proc = await asyncio.create_subprocess_shell(command, stderr=subprocess.PIPE)
+			_, stderr = await proc.communicate()
+
+			if stderr:
 				print(
-					f'An error has occured when setting fan speed to "{fanspeed_quantity.value.name}" with the following message \n {result.stderr}'
+					f'An error has occured when setting fan speed to "{fanspeed_quantity.value.name}" with the following message \n {stderr}'
 				)
